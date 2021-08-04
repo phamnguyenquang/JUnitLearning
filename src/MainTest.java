ee406d26ed133751d642f61f981699e46058584d
Quang <Quang@DESKTOP-R17B4T9>
2021-08-04 20:21:08 +0200

    First JU

---
+++ src/MainTest.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+//		ArrayList<Integer> arr = new ArrayList<Integer>();
+//		for (int i = 1; i < 100; ++i) {
+//			arr.add(i);
+//		}
+//		TestClass test = new TestClass(arr);
+//		System.out.println(test.getSum());
+		
+		
+	}
+
+}
